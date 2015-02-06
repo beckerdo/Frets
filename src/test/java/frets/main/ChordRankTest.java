cbf59d0dd4f27f8c5cd533716ca2ed9ff4230e62
Dan Becker <dabecker@paypal.com>
2015-02-06 13:52:01 -0600

    Update unit tests to support Mac OSX as well.

--- src/test/java/frets/main/ChordRankTest.java
+++ src/test/java/frets/main/ChordRankTest.java
@@ -3,28 +3,33 @@ package frets.main;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.junit.Test;
 
 /**
- * Unit tests to validate this class.  
+ * Unit tests to validate this class.
+ * 
  * @author <a href="mailto:dan@danbecker.info">Dan Becker</a>
  */
-public class ChordRankTest 
-{
-    @Test
-    public void testNullMock() 
-    {
-    	SimpleProperties<ChordRank> mock = ChordRank.instance.getInstance("");
-    	assertNull( "Null instance", mock );
-    }
-    
-    @Test
-    public void testMock() 
-    {
-    	SimpleProperties<ChordRank> mock = ChordRank.instance.getInstance( ChordRank.STANDARD );
-    	// System.out.println( "ChordRankTest name=\"" + mock.getName() + "\", description=\"" + mock.getDescription() + "\".");
-    	assertEquals( "Name", "Standard",  mock.getMetaName() );
-    	assertTrue( "Description", mock.getMetaDescription().startsWith("Ranks chords") );
-    	assertEquals( "Location", "src\\main\\resources\\frets\\main\\chordrank.standard.properties",  mock.getMetaLocation() );
-    }
+public class ChordRankTest {
+	public static final String PATH = File.separator;
+
+	@Test
+	public void testNullMock() {
+		SimpleProperties<ChordRank> mock = ChordRank.instance.getInstance("");
+		assertNull("Null instance", mock);
+	}
+
+	@Test
+	public void testMock() {
+		SimpleProperties<ChordRank> mock = ChordRank.instance.getInstance(ChordRank.STANDARD);
+		// System.out.println( "ChordRankTest name=\"" + mock.getName() +
+		// "\", description=\"" + mock.getDescription() + "\".");
+		assertEquals("Name", "Standard", mock.getMetaName());
+		assertTrue("Description", mock.getMetaDescription().startsWith("Ranks chords"));
+		String expected = "src" + PATH + "main" + PATH + "resources" + PATH + "frets" + PATH + "main" + PATH + "chordrank.standard.properties";
+		assertEquals("Location", expected, mock.getMetaLocation());
+	}
 }
